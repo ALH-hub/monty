d48260065a936c5f83904f485001b705be46d5b2
Alhadji Oumate <alhadjioum0@gmail.com>
2023-08-25 03:20:03 +0100

    main monty

--- monty_main.c
+++ monty_main.c
@@ -10,22 +10,15 @@
 
 int main(int argc, char **argv)
 {
-	FILE *file;
+	FILE *script_fd = NULL;
+	int exit_code = EXIT_SUCCESS;
 
 	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-
-	file = fopen(argv[1], "r");
-	if (file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	run_monty(file);
-
-	fclose(file);
-	return (EXIT_SUCCESS);
+		return (usage_error());
+	script_fd = fopen(argv[1], "r");
+	if (script_fd == NULL)
+		return (f_open_error(argv[1]));
+	exit_code = run_monty(script_fd);
+	fclose(script_fd);
+	return (exit_code);
 }
