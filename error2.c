18b9aa4be6aff49cb22e4c9dddaf8295b924befd
Alhadji Oumate <alhadjioum0@gmail.com>
2023-08-25 03:18:10 +0100

    error messages 2

---
+++ error2.c
@@ -0,0 +1,69 @@
+#include "monty.h"
+
+int short_stack_error(unsigned int line_number, char *op);
+int div_error(unsigned int line_number);
+int pop_error(unsigned int line_number);
+int div_error(unsigned int line_number);
+int pchar_error(unsigned int line_number, char *message);
+
+/**
+ * pop_error - pop error
+ * @line_number: Line number in monty script
+ *
+ * Return: EXIT_FAILURE
+ */
+int pop_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * pint_error - empty stack
+ * @line_number: Line number in Monty script
+ *
+ * Return: EXIT_FAILURE
+ */
+int pint_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * short_stack_error - small stack for operations
+ * @line_number: Line number in monty script
+ * @op: where error occured
+ *
+ * Return: EXIT_FAILURE
+ */
+int short_stack_error(unsigned int line_number, char *op)
+{
+	fprintf(stderr, "L%u: can't %s, stack too short\n", line_number, op);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * div_error - division by 0 error
+ * @line_number: Line number in monty script
+ *
+ * Return: EXIT_FAILURE
+ */
+int div_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: division by zero\n", line_number);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * pchar_error - empty stack and non char vals
+ * @line_number: Line number in monty script
+ * @message: error message to print
+ *
+ * Return: EXIT_FAILURE
+ */
+int pchar_error(unsigned int line_number, char *message)
+{
+	fprintf(stderr, "L%u: can't pchar, %s\n", line_number, message);
+	return (EXIT_FAILURE);
+}
